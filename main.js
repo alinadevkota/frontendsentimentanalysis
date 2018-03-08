7de0a54f3ce93c0b9168381c2934005101a4d25b
AlinaDevkota <072bct504@pcampus.edu.np>
2018-03-08 12:58:09 +0545

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+
+// JavaScript Document
+"use strict";
+
+//Global Variable Declaration
+
+function update(){
+	//Event Handlers and listeners
+	
+	document.getElementById("technology").onclick = function(){
+        
+		document.getElementById("content").data="technology.html";
+		
+    };
+    document.getElementById("home").onclick=function(){
+        
+        document.getElementById("content").data="home.html";
+    };
+    document.getElementById("categories").onclick=function(){
+        
+        document.getElementById("content").data="categories.html";
+    };
+    document.getElementById('search').addEventListener('keypress', function(event) {
+        if (event.keyCode == 13) {
+            alert("hi!");
+        }
+    });
+	
+}
+
+window.onload = function() {
+	// prep anything we need to
+    document.getElementById("content").data="home.html";
+		setInterval(update,1000);
+	
+};
